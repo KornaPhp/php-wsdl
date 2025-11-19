f700ff0450e547c6d0b743f4f1b9c49e2daa0cb9
Toon Verwerft <toonverwerft@gmail.com>
2025-11-19 07:33:29 +0100

    Support leauge/uri 7.6

--- src/Uri/IncludePathBuilder.php
+++ src/Uri/IncludePathBuilder.php
@@ -4,18 +4,34 @@ declare(strict_types=1);
 
 namespace Soap\Wsdl\Uri;
 
-use League\Uri\BaseUri;
 use League\Uri\Modifier;
+use League\Uri\Uri;
+use function Psl\Str\starts_with;
 
 final class IncludePathBuilder
 {
     public static function build(string $relativePath, string $fromFile): string
     {
-        return Modifier::from(BaseUri::from($fromFile)->resolve($relativePath))
+        $baseUri = match(true) {
+            starts_with($fromFile, '/') => Uri::fromUnixPath($fromFile),
+            default => Uri::new($fromFile),
+        };
+
+        $resolvedUri = $baseUri->resolve($relativePath);
+        $modifier = Modifier::wrap($resolvedUri)
             ->removeDotSegments()
-            ->removeEmptySegments()
-            ->getUri()
-            ->__toString()
-        ;
+            ->removeEmptySegments();
+
+        /**
+         * @var Uri $relativeUri
+         * @psalm-suppress UndefinedClass PHP's URI classes are only available from PHP 8.5
+         */
+        $relativeUri = $modifier->unwrap();
+
+        if ($relativeUri->getScheme() === 'file') {
+            return $relativeUri->getPath();
+        }
+
+        return $relativeUri->toString();
     }
 }
